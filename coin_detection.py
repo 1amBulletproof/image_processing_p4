dc57d3c5c47b8887fb2122e89cd62d062465705c
1amBulletproof <brandon.tarney@gmail.com>
2017-10-29 14:10:57 -0400

    cleanup for submission (remove prints and comments)

--- coin_detection.py
+++ coin_detection.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
-#Detect coins
-
-#Example found here: http://blog.christianperone.com/2014/06/simple-and-effective-coin-segmentation-using-python-and-opencv/:
+#Detect & Classify US Currency Coins
+#@Author:   Brandon Tarney
+#@Date:     10/29/2017
 
 from matplotlib import pyplot as plt
 import numpy as np
@@ -10,18 +10,17 @@ import glob, cv2
 
 def main():
     image_paths = glob.glob("./CoinImages/*.jpg")
-    #print images
 
     num_rows = 1
     num_cols = 3
     
-
     for image_path in image_paths:
         plot_pos = 1
         plt.figure(figsize=(13, 6))
 
         input_img = cv2.imread(image_path)
-        #display_img("input img", input_img)
+
+        #plot
         plt.subplot(num_rows, num_cols, plot_pos)
         plt.imshow(input_img, aspect='auto')
         plt.title('Original'), plt.axis('off')
@@ -29,41 +28,32 @@ def main():
     
         #Ensure the image is grayscale
         gray_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
-        #display_img("gray img", gray_img)
     
         #Blur the image to reduce the noise
-        #gausian, vs. mean, vs. blur (average)
         blurred_gray_img = cv2.blur(gray_img, (11, 11))
-        #display_img("blurred gray img", blurred_gray_img)
     
         #Adaptive threshold to control for different lighting but this will produce noise!
-        # gausian vs. mean!?
         threshold_mask = cv2.adaptiveThreshold(blurred_gray_img, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
                     cv2.THRESH_BINARY_INV, 7, 1)
     
-        #threshold_mask = cv2.Canny(blurred_gray_img, 50, 100)
-        #display_img("Threshold mask", threshold_mask)
     
+        #Remove the noise from the threshold so it's not expanded by closing
         kernel = np.ones((3, 3), np.uint8)
-    
-        #Remove the noise
         low_noise_mask = cv2.erode(threshold_mask, kernel, iterations=1)
-        #display_img("noise-removed mask", low_noise_mask)
     
         kernel2 = np.ones((3, 3), np.uint8)
     
         #Close (morphology): forms cohesive, closed shapes in the mask
         closed_mask = cv2.morphologyEx(low_noise_mask, cv2.MORPH_CLOSE,
                     kernel2, iterations=17)
-        #display_img("Closed mask", closed_mask)
     
         #Disconnect anyone who got connected
         eroded_mask = cv2.erode(closed_mask, kernel2, iterations=33)
-        #display_img("eroded - mask", eroded_mask)
     
-        #regain former shape  anyone who got connected
+        #regain former shape/size
         enlarged_mask = cv2.dilate(eroded_mask, kernel2, iterations=33)
-        #display_img("enlarged - mask", enlarged_mask)
+
+        #plot
         plt.subplot(num_rows, num_cols, plot_pos)
         plt.imshow(enlarged_mask, cmap='gray', aspect='auto')
         plt.title('Mask'), plt.axis('off')
@@ -82,9 +72,10 @@ def main():
                 #print(coin_details)
                 ellipse = cv2.fitEllipse(contour)
                 cv2.ellipse(input_img, ellipse, (0,255,0), 2)
-                cv2.putText(input_img, coin_type, (contour[0][0][0], contour[0][0][1]), cv2.FONT_HERSHEY_COMPLEX,2, (255,0, 0), 3)
+                cv2.putText(input_img, coin_type, (contour[0][0][0], contour[0][0][1]), 
+                        cv2.FONT_HERSHEY_COMPLEX,2, (255,0, 0), 3)
     
-        #display_img("final img", input_img)
+        #plot
         plt.subplot(num_rows, num_cols, plot_pos)
         plt.imshow(input_img, aspect='auto')
         plt.title('Final'), plt.axis('off')
